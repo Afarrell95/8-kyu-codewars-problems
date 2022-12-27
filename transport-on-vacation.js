d8411c0a611d418a5aa6822c523b9297c7c05f21
Allison01~ <farrella95@gmail.com>
2022-12-26 20:47:33 -0500

    new prob

---
+++ transport-on-vacation.js
@@ -0,0 +1,17 @@
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+    const total = d * 40
+    if(d <3){
+      return total
+    } else if(d > 6){
+      return total - 50
+    }else if(d >2){
+      return total -20
+    }
+  }
+
