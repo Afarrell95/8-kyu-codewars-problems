d51f78bf7fad6130b38200c2b842b6d2019abe25
Allison <farrella95@gmail.com>
2024-04-19 07:09:22 -0400

    fixed error

--- does-my-list0include.js
+++ does-my-list0include.js
@@ -1,12 +1,10 @@
 //Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
 function include(arr, item) {
-  function include(arr, item) {
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] == item) {
-        return true;
-      }
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
     }
-    return false;
   }
+  return false;
 }
